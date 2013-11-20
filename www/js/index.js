0873cadd0b1d46d45ae984834065903035d2b6b9
Joe Newton <joe.newton@reurgency.com>
2013-11-20 09:49:45 -0500

    Added my own sender ID

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"469781178849","ecb":"app.onNotificationGCM"});//Old SenderId = 824841663931
 
     },
     // Update DOM on a Received Event
